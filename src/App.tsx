493974baceead0fbcbf25d662b1ea558eb8c5696
Giorgi <grgpaikidze@gmail.com>
2023-02-05 20:16:23 +0400

    cleanup files

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,9 @@
-import { useState } from "react";
 import "./App.css";
-import AddToList from "./components/AddToList";
-import List from "./components/List";
-
-export interface IState {
-  people: {
-    name: string;
-    age: number;
-    url: string;
-    note?: string;
-  }[];
-}
 
 function App() {
-  const [people, setPeople] = useState<IState["people"]>([
-    {
-      name: "Lebron James",
-      age: 36,
-      url: "https://media-cldnry.s-nbcnews.com/image/upload/newscms/2021_16/3466843/210422-lebron_james-mc-1140.JPG",
-      note: "Pretty good player",
-    },
-  ]);
-
   return (
-    <div className="App">
-      <h1>People invited to my Party</h1>
-      <List people={people} />
-      <AddToList people={people} setPeople={setPeople} />
+    <div>
+      <h1>Hello</h1>
     </div>
   );
 }
